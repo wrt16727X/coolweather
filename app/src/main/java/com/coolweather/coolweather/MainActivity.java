cf3d355b4867f2dbbb9922e5f06062738a0303a6
wrt <718781594@qq.com>
2021-01-28 11:53:06 +0800

    First commit

--- app/src/main/java/com/coolweather/coolweather/MainActivity.java
+++ app/src/main/java/com/coolweather/coolweather/MainActivity.java
@@ -3,12 +3,16 @@ package com.coolweather.coolweather;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.tv);
     }
 }
